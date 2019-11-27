410ec2813fd8d89b09458363569f4cb765814b37
Ubuntu <ubuntu@ip-172-31-26-54.ec2.internal>
2019-11-27 20:38:46 +0000

    trying to get MongoDB going

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 import os
+import pymongo
 from flask import Flask
 from flask_pymongo import PyMongo
-from bson.objectId import ObjectId
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-#app.config['MONGO_DBNAME'] = 'MongoDb project name when ready for wiring'
-#app.config['MONGO_URI'] = 'This will be connection as provided by MongoDB'   // dont forget to hide password in bashrc and import from OS
+DBS_NAME = 'my_data_project'
+MONGODB_URI = os.getenv("MONGO_URI")
+
+
+app.route('/')
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
+        return conn
+        
+    except pymongo.errors.ConnectionFailure as e:
+        print("could not connect to MongoDB: %s") % e
 
 #mongo = PyMongo(app)
 
 
-app.route('/')
+
+
+
 
 
 if __name__ == '__main__':
