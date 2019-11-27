f466c682fdb0ec86c306d017a501a29c1f4f40c4
Ubuntu <ubuntu@ip-172-31-26-54.ec2.internal>
2019-11-27 21:34:59 +0000

    getting a 404 when I preview application

--- app.py
+++ app.py
@@ -6,29 +6,16 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-DBS_NAME = 'my_data_project'
-MONGODB_URI = os.getenv("MONGO_URI")
-
-
-app.route('/')
-def mongo_connect(url):
-    try:
-        conn = pymongo.MongoClient(url)
-        print("Mongo is connected")
-        return conn
-        
-    except pymongo.errors.ConnectionFailure as e:
-        print("could not connect to MongoDB: %s") % e
+#DBS_NAME = 'my_data_project'
+#MONGODB_URI = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 #mongo = PyMongo(app)
-
-
-
-
-
+app.route('/')
+def hello():
+    return "Hello World again!"
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
