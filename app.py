f71bbace72ec18b03b52655b417a2492583879f8
Ubuntu <ubuntu@ip-172-31-26-54.ec2.internal>
2019-11-28 19:26:36 +0000

    Uploading to heroku to get around application error

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 import os
-import pymongo
+##import pymongo
 from flask import Flask
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+#from flask_pymongo import PyMongo
+#from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 #DBS_NAME = 'my_data_project'
-#MONGODB_URI = os.getenv('MONGO_URI', 'mongodb://localhost')
+#MONGO_URI = os.getenv('MONGO_URI')
 
 #mongo = PyMongo(app)
+
 app.route('/')
 def hello():
-    return "Hello World again!"
+    return "Hello World.... again!"
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+        port=int(os.environ.get('PORT')),
+        debug=True)
