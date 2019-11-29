bef3d85907415fbbaf3b9d8b672239df8b776dcc
Ubuntu <ubuntu@ip-172-31-26-54.ec2.internal>
2019-11-29 19:45:03 +0000

    changing Procfile to Worker

--- app.py
+++ app.py
@@ -18,6 +18,6 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-    port=int('8080'),
+    app.run(host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT')),
     debug=True)
