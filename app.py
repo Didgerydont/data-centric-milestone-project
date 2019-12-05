1285cfc6aa70c9944706786880e746263f837816
Ubuntu <ubuntu@ip-172-31-26-54.ec2.internal>
2019-12-05 18:56:10 +0000

    tryi ng to connect mongodb

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import pymongo
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -10,7 +10,7 @@ DBS_NAME = 'my_data_project'
 app.config["MONGO_DBNAME"] = "my_data_project"
 MONGO_URI = os.getenv("MONGO_URI")
 
-#mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 @app.route("/")
 @app.route("/index")
@@ -21,7 +21,10 @@ def hello():
 ##def test():
 ##  return render_template('about.html')
 
-
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html', tasks=mongo.db.categories.find())
+    
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'),
