03ae14e5d06a8f6653b3e5fec6f99ccf149f547c
Didgerydont <prendygast@hotmail.com>
2019-12-31 08:38:52 +0000

    trying to change env variables but its not working, also unable to get heroku CLI working

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import pymongo
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = "my_data_project"
-app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+MONGO_URI = os.environ.get('MONGO_URI') 
 mongo = PyMongo(app)
 
 
@@ -20,7 +23,7 @@ def hello():
 @app.route('/readrecipe')
 def get_recipe():
     return render_template('readrecipe.html', 
-    dish_names=mongo.db.dish_names.find())
+    recipes=mongo.db.recipes.find())
     
 
 if __name__ == '__main__':
