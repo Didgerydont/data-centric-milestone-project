1c2b6e8a09d5e49df906cb32d41228a2f29fc367
Will Prendergast <prendygast@hotmail.com>
2020-01-05 16:23:05 +0000

    Adding log in system, installed bcrypt

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import pymongo
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import bcrypt
 if os.path.exists("env.py"):
     import env
 
@@ -12,11 +13,29 @@ app.config["MONGO_DBNAME"] = os.environ.get('my_data_project')
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
 
+username = mongo.db.users.find()
 
 @app.route("/")
 @app.route("/index")
 def home():
+    if 'username' in session:
+        return "You are logged in as " + session['username']
     return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('user_management.html')
+
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users=mongo.db.users
+        existing_user = users.find_one({'name': request.form['username']})
+
+    if existing_user == None:
+        hashpash = bcrypt.hashpw(request.form[''])
+
+    return ''
 
 
 @app.route('/readrecipe')
